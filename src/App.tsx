bb0a6e5a9652800de2f3a1f9e7152bf3dbbf386e
Sturion <sturion.douglas@gmail.com>
2023-08-11 15:38:45 -0300

    API Get

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from 'react-query'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface PersonData {
+  name: string;
+  phone: number;
+  email: string;
+}
 
+const queryClient = new QueryClient()
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   )
 }
 
-export default App
+function Example() {
+  const { isLoading, data } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/users').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return 'Loading...'
+
+  return (
+    <div>
+      {data.map((contato:PersonData) =>
+      <div>
+      <div>{contato.name}</div>
+      <div>{contato.phone}</div>
+      <div>{contato.email}</div>
+      <br />
+      </div>
+      )}
+      <h1>{data.name}</h1>
+    </div>
+  )
+}
